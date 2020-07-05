89e1df0ef0ea4711296d007f556f592756af2c5c
Michael <361733357@qq.com>
2020-07-05 22:39:52 +0800

    my stores

--- src/views/bvo/bvo-store-management/bvo-store-management.vue
+++ src/views/bvo/bvo-store-management/bvo-store-management.vue
@@ -1,8 +1,21 @@
 <template>
   <div style="margin: 60px; white-space:nowrap;">
 
-    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
-      <el-form-item>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm">
+
+      <h2>My Stores</h2>
+
+      <el-table :data="amazonData" style="width: 100%; margin-bottom: 30px" :header-cell-style="{color: '#0f81ff'}">
+        <el-table-column prop="name" label="Amazon">
+        </el-table-column>
+      </el-table>
+
+      <el-table :data="ebayData" style="width: 100%" :header-cell-style="{color: '#0f81ff'}">
+        <el-table-column prop="name" label="Ebay">
+        </el-table-column>
+      </el-table>
+
+      <el-form-item style="margin-top: 30px;">
         <el-button type="primary" @click="toChoose('ruleForm')">Add a new store</el-button>
       </el-form-item>
     </el-form>
@@ -13,7 +26,21 @@
   export default {
     data() {
       return {
-      };
+        amazonData: [{
+          name: 'store_001'
+        }, {
+          name: 'store_002'
+        },{
+          name: 'store_003'
+        }],
+        ebayData: [{
+          name: 'store_001'
+        }, {
+          name: 'store_002'
+        },{
+          name: 'store_003'
+        }]
+      }
     },
     methods: {
       toChoose(formName) {
