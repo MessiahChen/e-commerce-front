3f082937a275beca7d150853332f684567f290e1
Michael <361733357@qq.com>
2020-07-15 21:50:17 +0800

    bvo完成，目前mvo富文本

--- src/views/mvo/mvo-info/mvo-company-info.vue
+++ src/views/mvo/mvo-info/mvo-company-info.vue
@@ -28,47 +28,6 @@
           <el-input v-model="ruleForm.url" placeholder="Please enter the URL"></el-input>
         </el-col>
       </el-form-item>
-
-      <!-- 其他形式 -->
-<!--      <el-form-item label="活动区域" prop="region">
-        <el-select v-model="ruleForm.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="活动时间" required>
-        <el-col :span="11">
-          <el-form-item prop="date1">
-            <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
-          </el-form-item>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-form-item prop="date2">
-            <el-time-picker type="fixed-time" placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
-          </el-form-item>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="即时配送" prop="delivery">
-        <el-switch v-model="ruleForm.delivery"></el-switch>
-      </el-form-item>
-      <el-form-item label="活动性质" prop="type">
-        <el-checkbox-group v-model="ruleForm.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源" prop="resource">
-        <el-radio-group v-model="ruleForm.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式" prop="desc">
-        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
-      </el-form-item> -->
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">Save</el-button>
         <el-button @click="resetForm('ruleForm')">Reset</el-button>
@@ -80,6 +39,12 @@
 
 <script>
   import Tinymce from '@/components/Tinymce'
+  import {
+    getCompany,
+    initCompany,
+    updateCompany
+  } from '@/network/mvo-man-info.js'
+
   export default {
     components: { Tinymce },
     data() {
@@ -90,13 +55,6 @@
           intro:'',
           type:'',
           url:'',
-          // region: '',
-          // date1: '',
-          // date2: '',
-          // delivery: false,
-          // type: [],
-          // resource: '',
-          // desc: ''
         },
         rules: {
           companyNameCN: [{
@@ -124,55 +82,31 @@
             message: 'Please enter the GMC Report URL',
             trigger: 'blur'
           }]
-          // name: [{
-          //     required: true,
-          //     message: '请输入活动名称',
-          //     trigger: 'blur'
-          //   },
-          //   {
-          //     min: 3,
-          //     max: 5,
-          //     message: '长度在 3 到 5 个字符',
-          //     trigger: 'blur'
-          //   }
-          // ],
-          // region: [{
-          //   required: true,
-          //   message: '请选择活动区域',
-          //   trigger: 'change'
-          // }],
-          // date1: [{
-          //   type: 'date',
-          //   required: true,
-          //   message: '请选择日期',
-          //   trigger: 'change'
-          // }],
-          // date2: [{
-          //   type: 'date',
-          //   required: true,
-          //   message: '请选择时间',
-          //   trigger: 'change'
-          // }],
-          // type: [{
-          //   type: 'array',
-          //   required: true,
-          //   message: '请至少选择一个活动性质',
-          //   trigger: 'change'
-          // }],
-          // resource: [{
-          //   required: true,
-          //   message: '请选择活动资源',
-          //   trigger: 'change'
-          // }],
-          // desc: [{
-          //   required: true,
-          //   message: '请填写活动形式',
-          //   trigger: 'blur'
-          // }]
         }
       };
     },
     methods: {
+      getCompany() {
+        var getCompanyVO = {
+          // TODO
+          manId: '1'
+        }
+        return new Promise((resolve, reject) => {
+          getCompany(getCompanyVO).then(response => {
+            this.ruleForm.companyNameCN = response.data.nameCn;
+            this.ruleForm.companyNameEN = response.data.nameEn;
+            var intro = response.data.description;
+            console.log(intro);
+            console.log(response.data.description);
+            this.$refs.editor.setContent("intro");
+            this.ruleForm.type = response.data.gmcReportType;
+            this.ruleForm.url = response.data.gmcReportUrl;
+            resolve();
+          }).catch(error => {
+            reject(error);
+          })
+        })
+      },
       submitForm(formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
@@ -191,6 +125,9 @@
           path: '/mvo/myInfo'
         });
       }
+    },
+    mounted() {
+      this.getCompany();
     }
   }
 </script>
